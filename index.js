1ada33a84f5852c0a599b73253c18ea52b564f8b
Ignat1996 <decl011@tut.by>
2020-11-24 02:37:31 +0300

    tasks

---
+++ index.js
@@ -0,0 +1,78 @@
+class Map {
+  constructor(data) {
+    this.data = data
+
+      .split(";")
+      .map((i) => i.replace(/["]/g, ``).split(","))
+      .map((i) => ({
+        name: [i[0].replace(/\s/g, ``), i[1]].join(","),
+        latitude: i[2],
+        longitude: i[3],
+      }));
+  }
+
+  getCountry = (side) => {
+    switch (side.toLowerCase()) {
+      case "northernmost":
+        return this.data.reduce((acc, city) =>
+          acc.latitude > city.latitude ? acc : city
+        ).name;
+      case "southernmost":
+        return this.data.reduce((acc, city) =>
+          acc.latitude < city.latitude ? acc : city
+        ).name;
+      case "westernmost":
+        return this.data.reduce((acc, city) =>
+          acc.longitude > city.longitude ? acc : city
+        ).name;
+      case "easternmost":
+        return this.data.reduce((acc, city) =>
+          acc.longitude < city.longitude ? acc : city
+        ).name;
+      default:
+        return `There is no such side of the world.
+  You should choose: northernmost, southernmost, westernmost, easternmost `;
+    }
+  };
+
+  getNearCity = (latitude, longitude) => {
+    let index = 0;
+    let sumCordinats = Math.abs(latitude + longitude);
+    this.data.map((i, ind) => {
+      let currentAbsoluteSum =
+        Math.abs(+i.latitude - latitude) + Math.abs(+i.longitude - longitude);
+      if (sumCordinats > currentAbsoluteSum) {
+        index = ind;
+        sumCordinats = currentAbsoluteSum;
+      }
+    });
+    return this.data[index].name;
+  };
+
+  getStates() {
+    return [
+      ...new Set(this.data.map((i) => i.name.replace(/(\D*?) /g, ""))),
+    ].join(" ");
+  }
+}
+
+const map = new Map(`
+"Nashville, TN", 36.17, -86.78;
+
+"New York, NY", 40.71, -74.00;
+
+"Atlanta, GA", 33.75, -84.39;
+
+"Denver, CO", 39.74, -104.98;
+
+"Seattle, WA", 47.61, -122.33;
+
+"Los Angeles, CA", 34.05, -118.24;
+
+"Memphis, TN", 35.15, -90.05`);
+
+console.log(map.getCountry("southernmost"));
+console.log(map.getCountry("Westernmost"));
+console.log(map.getNearCity(46, -120));
+console.log(map.getNearCity(34, -117));
+console.log(map.getStates());
